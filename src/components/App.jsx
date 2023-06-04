2d967679b4870b9b094eac974543967a758f601e
ruudyaa <martarudnytska26@gmail.com>
2023-06-05 02:44:55 +0300

    Imports all components and adds styles.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+import { Searchbar } from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Layout } from './Layout/Layout';
+
+export default class App extends Component {
+  state = {
+    textSearch: '',
+  };
+
+  // custom methods
+  handleSubmit = textSearch => {
+    this.setState({ textSearch });
+  };
+
+  render() {
+    const { textSearch } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSubmit} />
+        <Layout>
+          <ImageGallery value={textSearch} />
+        </Layout>
+        <ToastContainer transition={Slide} draggablePercent={60} />
+        <GlobalStyle />
+      </>
+    );
+  }
+}
