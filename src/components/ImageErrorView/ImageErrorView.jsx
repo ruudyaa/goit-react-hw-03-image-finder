754f5aad2dfb64112cda8eba31bf094dfd9b58e4
ruudyaa <martarudnytska26@gmail.com>
2023-06-05 02:43:18 +0300

    Adds ImageErrorView component and its styles.

---
+++ src/components/ImageErrorView/ImageErrorView.jsx
@@ -0,0 +1,14 @@
+import { Text, Wrapper } from './ImageErrorView.styled';
+import PropTypes from 'prop-types';
+
+export default function ImageErrorView({ message }) {
+  return (
+    <Wrapper role="alert">
+      <Text>{message}</Text>
+    </Wrapper>
+  );
+}
+
+ImageErrorView.propTypes = {
+  message: PropTypes.string,
+};
---
+++ src/components/ImageErrorView/ImageErrorView.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 32px;
+  margin: 1vh auto;
+`;
+
+export const Text = styled.p`
+  max-width: 80%;
+  color: ${props => props.theme.colors.accent};
+  text-shadow: ${props => props.theme.shadows.textShadow};
+
+  text-align: center;
+  
+  font-family: ${props => props.theme.fonts.monospace}
+  font-size: ${props => props.theme.fontSizes.l}
+`;
