b28b5e45f6e9b9bedc09034545975a57ff9e2d39
ruudyaa <martarudnytska26@gmail.com>
2023-06-07 02:54:01 +0300

    Update getImages.js

--- src/services/getImages.js
+++ src/services/getImages.js
@@ -1,23 +1,27 @@
-const BASE_URL = `https://pixabay.com/api/`;
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '35648673-0e16e427271b01d9e7b771895';
 
-const getImages = (searchText, page = 1) => {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(
-      new Error(
-        `Sorry, there are no ${searchText} images matching your search... `
-      )
-    );
+export const getImages = async (query, page) => {
+  const OPTIONS = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: page,
+    per_page: 12,
   });
+  try {
+    const response = await axios.get(`?${OPTIONS.toString()}`);
+    return response.data;
+  } catch (error) {
+    console.error(error.toJSON());
+  }
 };
 
-const api = {
-  getImages,
-};
-
-export default api;
+export const normalizedImages = imagesArray =>
+  imagesArray.map(({ id, tags, webformatURL, largeImageURL }) => {
+    return { id, tags, webformatURL, largeImageURL };
+  });
