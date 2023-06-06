b30aee8c4b28bd198455efa382c833d300cc3bef
ruudyaa <martarudnytska26@gmail.com>
2023-06-07 02:54:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,104 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
+import * as API from 'services/getImages.js';
+
+import { ToastContainer, Slide, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Searchbar } from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
+import { ImageGallery } from './ImageGallery/ImageGallery';
 
-import { ToastContainer, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Loader } from './Loader/Loader';
+import { Button } from 'components/Button/Button';
 import { Layout } from './Layout/Layout';
+
+const notifyOptions = {
+  position: 'top-center',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+};
 
 export default class App extends Component {
   state = {
-    textSearch: '',
+    searchName: '',
+    images: [],
+    currentPage: 1,
+    error: null,
+    isLoading: false,
+    totalPages: 0,
   };
 
-  // custom methods
-  handleSubmit = textSearch => {
-    this.setState({ textSearch });
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.searchName !== this.state.searchName ||
+      prevState.currentPage !== this.state.currentPage
+    ) {
+      this.addImages();
+    }
+  }
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+    }));
+  };
+
+  handleSubmit = query => {
+    this.setState({
+      searchName: query,
+      images: [],
+      currentPage: 1,
+    });
+  };
+
+  addImages = async () => {
+    const { searchName, currentPage } = this.state;
+    try {
+      this.setState({ isLoading: true });
+
+      const data = await API.getImages(searchName, currentPage);
+
+      if (data.hits.length === 0) {
+        return toast.error(
+          'Sorry, there are no images matching your search',
+          notifyOptions
+        );
+      }
+      const normalizedImages = API.normalizedImages(data.hits);
+      this.setState(state => ({
+        images: [...state.images, ...normalizedImages],
+        isLoading: false,
+        error: '',
+        totalPages: Math.ceil(data.totalHits / 12),
+      }));
+    } catch (error) {
+      this.setState({ error: 'Something went wrong!' });
+    } finally {
+      this.setState({ isLoading: false });
+    }
   };
 
   render() {
-    const { textSearch } = this.state;
+    const { images, isLoading, currentPage, totalPages } = this.state;
 
     return (
-      <>
+      <div>
         <Searchbar onSubmit={this.handleSubmit} />
         <Layout>
-          <ImageGallery value={textSearch} />
+          <ImageGallery images={images} />
         </Layout>
+        {isLoading && <Loader />}
+        {images.length > 0 && totalPages !== currentPage && !isLoading && (
+          <Button onClick={this.loadMore}>Load More</Button>
+        )}
         <ToastContainer transition={Slide} draggablePercent={60} />
         <GlobalStyle />
-      </>
+      </div>
     );
   }
 }
